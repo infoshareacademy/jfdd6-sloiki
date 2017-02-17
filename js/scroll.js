aea33a35227b6a545bcb00d135448f9dcf0ee546
Marta Rzadkowska <marta.rzadkowska@gmail.com>
2017-02-17 15:21:31 +0100

    adding product, team and form

--- js/scroll.js
+++ js/scroll.js
@@ -1,45 +1,39 @@
-$('.menu--content .container').on('click', function (event) {
-  event.preventDefault();
 
-  var href = $('#').attr('href')
+
+$('.nav-product').on('click', function (event) {
+  event.preventDefault();
+  var p = $('#product');
   var offset = p.offset().top;
+
+  $('html, body').animate({
+    scrollTop: offset
+  }, 1000);
+});
+
+$('.nav-team').on('click', function (event) {
+  event.preventDefault();
+  var p = $('#team');
+  var offset = p.offset().top;
+
+  $('html, body').animate({
+    scrollTop: offset
+  }, 1000);
+});
+
+$('.nav-form').on('click', function (event) {
+  event.preventDefault();
+  var p = $('#form');
+  var offset = p.offset().top;
+
+  $('html, body').animate({
+    scrollTop: offset
+  }, 1000);
+});
+
+$('.header--menu').on('click', function (event) {
+  event.preventDefault();
 
   $('html, body').animate({
     scrollTop: 0
   }, 1000);
 });
-
-
-$('.menu--nav a').on('click', function (event) {
-  event.preventDefault();
-
-  var href = $('#product').attr('href')
-  var offset = p.offset().top;
-
-  $('html, body').animate({
-    scrollTop: offset
-  }, 1000);
-});
-
-$('.menu--nav a').on('click', function (event) {
-  event.preventDefault();
-
-  var href = $('#team').attr('href')
-  var offset = p.offset().top;
-
-  $('html, body').animate({
-    scrollTop: offset
-  }, 1000);
-});
-
-$('.menu--nav a').on('click', function (event) {
-  event.preventDefault();
-
-  var href = $('#form').attr('href')
-  var offset = p.offset().top;
-
-  $('html, body').animate({
-    scrollTop: offset
-  }, 1000);
-});
-
