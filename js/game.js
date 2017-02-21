8c4ac6f670fc7054c211d3d6f4f1d4796d7033c0
Karol Jakubcewicz <karol.jakubcewicz@gmail.com>
2017-02-21 16:36:20 +0100

    FD6SL-39 table with cards created

--- js/game.js
+++ js/game.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
 
-  var $conteiner = $('.table');
+  var $container = $('.game--board');
   var $table = $('<table>');
   var $tr;
   var $td;
 
-  $conteiner.append($table);
+  $container.append($table).addClass('game--board');
 
 
   for (var y = 0; y < 4; y += 1) {
     $tr = $('<tr>');
 
-    for(var x=0; x<4; x +=1) {
-      $td = $('<td>').addClass('cell cover');
+    for(var x = 0; x < 4; x += 1) {
+      $td = $('<td>').addClass('game--card');
       $tr.append($td);
     }
     $table.append($tr);
