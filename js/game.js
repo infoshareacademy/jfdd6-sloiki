ef0c387f35fc705003701e0e72b848ee527476e5
Karol Jakubcewicz <karol.jakubcewicz@gmail.com>
2017-02-22 16:28:24 +0100

    random classes added to cards

--- js/game.js
+++ js/game.js
@@ -5,21 +5,44 @@ $(document).ready(function () {
   var $tr;
   var $td;
 
-  var cards = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  var cards = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
+  // We create board
   $container.append($table).addClass('game--board');
 
-
+  // We create empty fields
   for (var y = 0; y < 4; y += 1) {
     $tr = $('<tr>');
 
     for(var x = 0; x < 4; x += 1) {
-      $td = $('<td>').addClass('game--card')
-        .on('click', function () {
-          $(this).toggleClass('game--card__flipped');
-        });
+      $td = $('<td>');
       $tr.append($td);
     }
     $table.append($tr);
   }
+
+  // We put cards on fields
+  cards.forEach(function(cardName) {
+    putCard(cardName, $table);
+    putCard(cardName, $table);
+  });
+
+  // It puts card on empty field on $table
+  function putCard(cardName, $table) {
+    var $emptyFields = $('td:not(.game--card)', $table);
+    var howManyEmptyFields = $emptyFields.length;
+
+    $emptyFields
+      .eq(Math.floor(Math.random() * howManyEmptyFields))
+      .addClass('game--card')
+      .addClass('game--card__covered')
+      .addClass(cardName)
+      .on('click', function () {
+        $(this).removeClass('game--card__covered');
+      });
+  }
+
+
+
+
 });
