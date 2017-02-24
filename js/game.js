a6510ece910cc6c52b5c4b67d4bdb6ae2ad15586
Karol Jakubcewicz <karol.jakubcewicz@gmail.com>
2017-02-24 16:42:10 +0100

    todos added in game.js

--- js/game.js
+++ js/game.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
 
   var cards = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
+  //TODO: funkcja wykrywająca kliknięcie na przycisk "Wyślij" -> pokazanie (stworzenie planszy do gry)
+
   // We create board
   $container.append($table).addClass('game--board');
 
@@ -22,54 +24,54 @@ $(document).ready(function () {
     $table.append($tr);
   }
 
-  // We put cards on fields
-  cards.forEach(function(cardName) {
-    putCard(cardName, $table);
-    putCard(cardName, $table);
-  });
 
-  // It puts card on empty field on $table
-  function putCard(cardName, $table) {
-    var $emptyFields = $('td:not(.game--card)', $table);
-    var howManyEmptyFields = $emptyFields.length;
+  //TODO: licznik kliknięć dodany do DOM
+  //TODO: przycisk podpięty pod funkcję restartGame - dodany na tym etapie do DOM
 
-    $emptyFields
-      .eq(Math.floor(Math.random() * howManyEmptyFields))
-      .addClass('game--card')
-      .addClass('game--card__covered')
-      .addClass(cardName)
-      .attr('data-card', cardName)
-      .on('click', function () {
-        points += 1;
-        var $uncoveredCards = $('td:not(.game--card__covered)', $table);
-        var howManyUncovered = $uncoveredCards.length;
+  // function newGame() {
+    // We put cards on fields
+    cards.forEach(function(cardName) {
+      putCard(cardName, $table);
+      putCard(cardName, $table);
+    });
 
-        console.log('Liczba odkrytych kart to:', howManyUncovered);
+    // It puts card on empty field on $table
+    function putCard(cardName, $table) {
+      var $emptyFields = $('td:not(.game--card)', $table);
+      var howManyEmptyFields = $emptyFields.length;
 
-        if (howManyUncovered === 2) {
-          if ($uncoveredCards.first().attr('data-card') == $uncoveredCards.last().attr('data-card')) {
-            $uncoveredCards.addClass('correct')
-          }
-          else {
+      $emptyFields
+        .eq(Math.floor(Math.random() * howManyEmptyFields))
+        .addClass('game--card')
+        .addClass('game--card__covered')
+        .addClass(cardName)
+        .attr('data-card', cardName)
+        .on('click', function () {
+          points += 1;
+          var $uncoveredCards = $('td:not(.game--card__covered)', $table);
+          var howManyUncovered = $uncoveredCards.length;
+
+          console.log('Liczba odkrytych kart to:', howManyUncovered);
+
+          if (howManyUncovered === 2) {
+            if ($uncoveredCards.first().attr('data-card') == $uncoveredCards.last().attr('data-card')) {
+              $uncoveredCards.addClass('correct')
+            }
+            else {
+              $uncoveredCards.addClass('game--card__covered')
+            }
+
             $uncoveredCards.addClass('game--card__covered')
           }
 
-          $uncoveredCards.addClass('game--card__covered')
-        }
+          $(this).removeClass('game--card__covered');
+        });
+    }
+  // }
 
-        $(this).removeClass('game--card__covered');
-      });
-  }
-
-
-  //funkcja end game - resetuje zmienne, wyczyści karty, listenery
-
-  // przycisk podpięty pod funkcję
-
-
-  //licznik kliknięć w td?
-
-//button nowa gra / restart
+  //function restartGame () {
+  //   //TODO: resetuje zmienne (w tym licznik kliknięć), wyczyści karty, listenery
+  // }
 
 
 });
